782a0859dfee9e6c303332e15eac6f4a26503502
sankooc <sankooc@163.com>
2019-12-28 23:45:05 +0800

    add close

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ```
 
 const convert = require('bezier-interpolation');
-const points = [{ x: 40, y: 120 }, { x: 100, y: 180 }, { x: 220, y: 130 }, { x: 400, y: 170 }, { x: 550, y: 120 }];
+const points = [{ x: 70, y: 120 }, { x: 150, y: 180 }, { x: 270, y: 130 }, { x: 440, y: 170 }, { x: 400, y: 60 }];
 const params = convert(points);
 console.log(params);
 
@@ -20,4 +20,6 @@ console.log(params);
 
 [demo](demo/)
 
-![demo](demo/demo.png)
+![close_graph](demo/close.png)
+
+![open_graph](demo/open.png)
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+Array.prototype.gt = function(inx) {
+  const it = inx % this.length;
+  if(it >= 0 ) return this[it];
+  return this[this.length + it]
+};
+
 const convert = (points, close = false, radio = 0.88) => {
   if(points.length < 3) throw new Error('more than 2');
   const mid = (p1, p2) => ({ x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2 });
@@ -5,7 +12,7 @@ const convert = (points, close = false, radio = 0.88) => {
   const dis = (p1, p2) => Math.sqrt(Math.pow(p1.y - p2.y, 2) + Math.pow(p1.x - p2.x, 2));
 
   const inc = (p, vector, head, radio) => {
-    console.log(radio);
+    // console.log(radio);
     let { x, y } = p;
     const { arc, div, len } = vector;
     if(head) {
@@ -20,39 +27,53 @@ const convert = (points, close = false, radio = 0.88) => {
     return { x, y };
   };
 
-  const as = [];
-  for(let i = 1; i < points.length; i += 1) {
-    as.push(mid(points[i - 1], points[i]));
+  const midpoints = [];
+  for(let i = 1; i <= points.length; i += 1) {
+    midpoints.push(mid(points.gt(i - 1), points.gt(i)));
   }
+  console.log(midpoints.length);
+  console.log(midpoints);
   const vectors = [];
-  for(let i = 1; i < as.length; i += 1) {
-    const arc = (as[i].y - as[i - 1].y) / (as[i].x - as[i - 1].x);
-    const dp1 = dis(points[i - 1], points[i]);
-    const dp2 = dis(points[i + 1], points[i]);
-    const div = dp1 / (dp2 + dp1);
-    const len = (as[i].x - as[i - 1].x);
+  for(let i = 0; i < midpoints.length; i += 1) {
+    const arc = (midpoints.gt(i + 1).y - midpoints.gt(i).y) / (midpoints.gt(i + 1).x - midpoints.gt(i).x);
+    // const dp1 = dis(points[i - 1], points[i]);
+    // const dp2 = dis(points[i + 1], points[i]);
+    // const div = dp1 / (dp2 + dp1);
+    const div = 0.5;
+    const len = (midpoints.gt(i + 1).x - midpoints.gt(i).x);
     vectors.push({ arc, len, div });
   }
+
   const params = [];
-  for(let i = 0; i < points.length - 1; i += 1) {
-    const c = points[i];
-    const n = points[i + 1];
-    if(i === 0) {
-      const v = vectors[0];
-      const h = inc(n, v, true, radio);
-      params.push([c.x, c.y, h.x, h.y, h.x, h.y, n.x, n.y]);
-    } else if(i === points.length - 2) {
-      const v = vectors[i - 1];
-      const h = inc(c, v, false, radio);
-      params.push([c.x, c.y, h.x, h.y, h.x, h.y, n.x, n.y]);
-    } else {
-      const v1 = vectors[i - 1];
-      const v2 = vectors[i];
+  const plen = close ? points.length : points.length - 1;
+  for(let i = 0; i < plen; i += 1) {
+    const c = points.gt(i);
+    const n = points.gt(i + 1);
+    if(close) {
+      const v1 = vectors.gt(i - 1);
+      const v2 = vectors.gt(i);
       const h1 = inc(c, v1, false, radio);
       const h2 = inc(n, v2, true, radio);
       params.push([c.x, c.y, h1.x, h1.y, h2.x, h2.y, n.x, n.y]);
+    } else {
+      if(i === 0) {
+        const v = vectors[0];
+        const h = inc(n, v, true, radio);
+        params.push([c.x, c.y, c.x, c.y, h.x, h.y, n.x, n.y]);
+      } else if(i === plen - 1) {
+        const v = vectors.gt(i - 1);
+        const h = inc(c, v, false, radio);
+        params.push([c.x, c.y, h.x, h.y, n.x, n.y, n.x, n.y]);
+      } else {
+        const v1 = vectors.gt(i - 1);
+        const v2 = vectors.gt(i);
+        const h1 = inc(c, v1, false, radio);
+        const h2 = inc(n, v2, true, radio);
+        params.push([c.x, c.y, h1.x, h1.y, h2.x, h2.y, n.x, n.y]);
+      }
     }
   }
+  // return { midpoints, points: params };
   return params;
 };
 
